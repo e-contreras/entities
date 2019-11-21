4c292722722072fbb3c129775e26f396d68bbc8c
Gino Junchaya <gjunchaya@consultoresinformaticos.com.py>
2019-11-21 13:52:04 -0300

    Se agregar tarjeta

---
+++ src/main/java/py/com/econtreras/entity/Tarjetas.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.com.econtreras.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tarjetas", catalog = "econtreras", schema = "", uniqueConstraints = {@UniqueConstraint(columnNames = {"numero"})})
+public class Tarjetas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 16)
+    @Column(name = "numero", nullable = false, length = 16)
+    private String numero;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "nombre", nullable = false, length = 45)
+    private String nombre;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha_expiracion_mes", nullable = false)
+    private int fechaExpiracionMes;
+    
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha_expiracion_anho", nullable = false)
+    private int fechaExpiracionAnho;
+    
+    @JoinColumn(name = "cliente", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Client cliente;
+
+    
+}
